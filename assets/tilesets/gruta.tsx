13b3fd64b8560fcff5730837075dda831e5f6b8f
felihenrique <felihenrique@gmail.com>
2024-01-10 14:16:59 -0300

    maps

---
+++ assets/tilesets/gruta.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="gruta" tilewidth="32" tileheight="32" tilecount="1008" columns="24">
+ <image source="gruta.png" width="768" height="1370"/>
+ <wangsets>
+  <wangset name="terrain" type="corner" tile="-1"/>
+ </wangsets>
+</tileset>
